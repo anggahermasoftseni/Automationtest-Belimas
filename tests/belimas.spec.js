6bcfd53d6a4884be2036e6e7dd03c996f13c9a28
anggahermasoftseni <angga.hermawan@softwareseni.com>
2025-03-18 08:36:48 +0700

    Upload file test (after reset)

---
+++ tests/belimas.spec.js
@@ -0,0 +1,31 @@
+import { test, expect } from "@playwright/test";
+const data = require("./resources/data.json")
+
+test("TC4.1.0 - Open the web page", async({page}) => {
+    // Go to web page
+    await page.goto("/");
+
+    // Expect a title to contain "Belimas"
+    await expect(page).toHaveTitle("Belimas - Simba Refinery")
+} );
+
+test("TC4.1.1 - Login with valid email and correct password", async({page}) => {
+    // Go to web page
+    await page.goto("/");
+
+    // Clik Masuk button
+    await page.locator('//span[text()="Masuk"]').click();
+
+    // Fill User Name
+    await page.locator('//input[@placeholder="Email"]').fill(data.userName);
+
+    // Fill Password
+    await page.locator('//input[@placeholder="Masukkan kata sandi anda"]').fill(data.password);
+
+    // Click Masuk Button
+    await page.locator('//span[text()="Masuk"]').click();
+
+
+    // Pause to keep the browser open
+    await page.pause();
+} );
